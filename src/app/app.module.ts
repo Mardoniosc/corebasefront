0594538c63ff0bc10c41989e20fbb4a08bc87b18
Mardoniosc <mardonio@live.com>
2020-06-09 18:07:07 -0300

    :v: Feito a tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { LoginModule, LoginRoutingModule } from './modules';
+
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    LoginModule,
+    LoginRoutingModule,
+    RouterModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/autentication/login/login.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LoginComponent, LogarComponent } from './components';
+
+@NgModule({
+  declarations: [LoginComponent, LogarComponent],
+  imports: [CommonModule, RouterModule],
+})
+export class LoginModule {}

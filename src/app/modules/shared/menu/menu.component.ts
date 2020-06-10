a18c241c667efe3243837e5a8c2b8d419fa7c19a
Mardoniosc <mardonio@live.com>
2020-06-10 17:23:56 -0300

    Montado o menu

--- src/app/modules/shared/menu/menu.component.ts
+++ src/app/modules/shared/menu/menu.component.ts
@@ -12,78 +12,10 @@ export class MenuComponent implements OnInit {
 
   permissoes: Permissoes[];
 
-  // variaveis de Menu
-
-  homeMenu = false;
-
-  dashboardMenu = false;
-
-  // usuario
-  usuarioPrincipal = false;
-
-  usuarioPesquisar = false;
-
-  usuarioCadastrar = false;
-
-  // Permissao
-  permissaoPrincipal = false;
-
-  permissaoPesquisar = false;
-
-  permissaoCadastrar = false;
-
-  // Perfil
-  perfilPrincipal = false;
-
-  perfilPesquisar = false;
-
-  perfilCadastrar = false;
-
   constructor(private storange: StorangeService) {}
 
   ngOnInit(): void {
     this.usuario = this.storange.getLocalUser();
     this.permissoes = this.usuario.perfil.permissoes;
-    this.carregaMenu();
-  }
-
-  carregaMenu() {
-    this.permissoes.map((x) => {
-      return this.habilitarOpcaoMenu(x.descricao);
-    });
-  }
-
-  habilitarOpcaoMenu(descricao) {
-    switch (descricao) {
-      case 'Usuário':
-        this.usuarioPrincipal = true;
-        break;
-      case 'Cadastrar Usuário':
-        this.usuarioCadastrar = true;
-        break;
-      case 'Pesquisar Usuário':
-        this.usuarioPesquisar = true;
-        break;
-      case 'Perfil':
-        this.perfilPrincipal = true;
-        break;
-      case 'Cadastrar Perfil':
-        this.perfilCadastrar = true;
-        break;
-      case 'Pesquisar Perfil':
-        this.perfilPesquisar = true;
-        break;
-      case 'Permissão':
-        this.permissaoPrincipal = true;
-        break;
-      case 'Cadastrar Permissão':
-        this.permissaoCadastrar = true;
-        break;
-      case 'Pesquisar Permissão':
-        this.permissaoPesquisar = true;
-        break;
-      default:
-        break;
-    }
   }
 }
--- src/app/modules/shared/models/permissoes.model.ts
+++ src/app/modules/shared/models/permissoes.model.ts
@@ -3,4 +3,5 @@ export interface Permissoes {
   descricao: string;
   url: string;
   permissaoPai?: Permissoes;
+  icone?: string;
 }

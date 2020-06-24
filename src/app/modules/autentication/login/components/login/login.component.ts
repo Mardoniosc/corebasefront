adeb28776faee1b3bf08c2d5f616ca6f3ae0ee42
Mardoniosc <mardonio@live.com>
2020-06-24 11:53:12 -0300

    Atualizado pegando info de usuário que loga na aplicação

--- src/app/modules/autentication/login/components/login/login.component.ts
+++ src/app/modules/autentication/login/components/login/login.component.ts
@@ -12,6 +12,7 @@ import {
   ErroDTO,
   UserLoggedService,
   StorangeService,
+  InfoUserLogged,
 } from 'src/app/modules/shared';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -26,6 +27,8 @@ export class LoginComponent implements OnInit {
   private subscriptions: Subscription[] = [];
 
   login = {} as Login;
+
+  infoUserLogged = {} as InfoUserLogged;
 
   erroGeral = {} as ErroGeral;
 
@@ -69,6 +72,9 @@ export class LoginComponent implements OnInit {
   }
 
   logar(): void {
+    this.loginService.pegaIpUser().subscribe((data) => {
+      this.infoUserLogged = data;
+    });
     this.spinner.show();
     if (this.form.invalid) {
       this.snackBar.open(

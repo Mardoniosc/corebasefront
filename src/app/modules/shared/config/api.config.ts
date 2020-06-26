ff814d46639dc74ed1ba617c382942350237ff9e
Mardoniosc <mardonio@live.com>
2020-06-26 17:52:05 -0300

    Mandando para prod

--- src/app/modules/shared/config/api.config.ts
+++ src/app/modules/shared/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: '/api',
-  // baseUrl: 'https://core-base-springboot-api.herokuapp.com',
+  // baseUrl: '/api',
+  baseUrl: 'https://core-base-springboot-api.herokuapp.com',
   pegaIp: 'http://ip-api.com/json',
 };
